994498d2a44fa4a43b8a04a9fe9aedf0923a953f
abheist <abhishek291093@gmail.com>
2020-04-06 23:15:07 +0530

    :construction: configured the authenticated user landing page

--- src/components/appHandler/AuthenticatedApp.js
+++ src/components/appHandler/AuthenticatedApp.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Home from '../home/Home';
+import Profile from '../profile/Profile';
+import WriteUp from '../writeUp/WriteUp';
+import { Link, Switch, Route } from 'react-router-dom';
+import Header from '../header/Header';
+import { Flex } from '../styles/Flex';
+import styled from 'styled-components/macro';
 
 function AuthenticatedApp() {
-	return <p>Authenticated</p>;
+	return (
+		<Flex flex="1" justify="center">
+			<Flex flex="1" direction="column" maxWidth="1200">
+				<Header />
+
+				<Switch>
+					<Route path="/profile">
+						<Profile />
+					</Route>
+					<Route path="/write">
+						<WriteUp />
+					</Route>
+					<Route exact path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Flex>
+		</Flex>
+	);
 }
 
 export default AuthenticatedApp;
