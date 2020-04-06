0d3945cbfccb4b1308670d29f2c3a82db5e61e73
abheist <abhishek291093@gmail.com>
2020-04-06 23:14:33 +0530

    :sparkles: new tooltip component with new style

---
+++ src/components/tooltip/Tooltip.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+	useTooltipState,
+	Tooltip as ReakitTooltip,
+	TooltipReference
+} from 'reakit/Tooltip';
+import styled from 'styled-components/macro';
+import { COLOR_LIGHT, COLOR_DARK } from '../styles/ThemeConstants';
+
+function Tooltip({ children, title, ...props }) {
+	const tooltip = useTooltipState();
+	return (
+		<>
+			<TooltipReference {...tooltip} {...children.props}>
+				{referenceProps => React.cloneElement(children, referenceProps)}
+			</TooltipReference>
+			<ReakitTooltip
+				{...tooltip}
+				{...props}
+				css={`
+					font-size: 12px;
+					padding: 4px 8px;
+					background: ${COLOR_DARK};
+					color: ${COLOR_LIGHT};
+					border-radius: 20px;
+				`}
+			>
+				{title}
+			</ReakitTooltip>
+		</>
+	);
+}
+
+export default Tooltip;
