3f4c21f318dcda2b4476ed05aa3879b187f64b69
abheist <abhishek291093@gmail.com>
2020-04-09 21:46:55 +0530

    ⏪ reverted the writebox

--- src/components/writeBox/writeBox.js
+++ src/components/writeBox/writeBox.js
@@ -9,27 +9,7 @@ import styled from 'styled-components/macro';
 import { MeH1, MeSubtitle1 } from '../styles/Typography';
 
 function WriteBox() {
-	return (
-		<div
-			css={`
-				background: ${COLOR_LIGHT};
-				width: 100%;
-				padding: 16px 24px 24px;
-				background: ${COLOR_LIGHT};
-				border-radius: 8px;
-				box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-			`}
-		>
-			<div
-				css={`
-					height: 60px;
-					background: ${COLOR_LIGHT_GRAY};
-					background-color: ${COLOR_DANGER_LIGHT}
-					border-radius: 20px;
-				`}
-			></div>
-		</div>
-	);
+	return <div>Dashboard</div>;
 }
 
 export default WriteBox;
