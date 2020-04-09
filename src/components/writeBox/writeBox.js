f08c3516acdef3e868e4c5ac35b12d291f925507
abheist <abhishek291093@gmail.com>
2020-04-09 21:47:53 +0530

    ♻️ intermediate refactoring

--- src/components/writeBox/writeBox.js
+++ src/components/writeBox/writeBox.js
@@ -1,12 +1,4 @@
 import React from 'react';
-import {
-	COLOR_LIGHT,
-	COLOR_LIGHT_GRAY,
-	COLOR_DANGER_LIGHT
-} from '../styles/ThemeConstants';
-// eslint-disable-next-line no-unused-vars
-import styled from 'styled-components/macro';
-import { MeH1, MeSubtitle1 } from '../styles/Typography';
 
 function WriteBox() {
 	return <div>Dashboard</div>;
