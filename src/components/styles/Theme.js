134d744ed939a8baadabdfc03d44d7a4dd381c00
abheist <abhishek291093@gmail.com>
2020-04-10 12:07:00 +0530

    🚧 intermediate

--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components/macro';
-import theme from 'styled-theming';
+import React from "react";
+import PropTypes from "prop-types";
+import { createGlobalStyle, ThemeProvider } from "styled-components/macro";
+import theme from "styled-theming";
 import {
-	COLOR_LIGHT,
-	COLOR_DARK,
-	FONT_SAN_SERIF,
-	FONT_MONO_SPACED,
-	COLOR_PRIMARY_DARK,
-	COLOR_PRIMARY,
-	COLOR_LIGHT_GRAY,
-	COLOR_PRIMARY_DARKER
-} from './ThemeConstants';
+  COLOR_LIGHT,
+  COLOR_DARK,
+  FONT_SAN_SERIF,
+  FONT_MONO_SPACED,
+  COLOR_PRIMARY_DARK,
+  COLOR_PRIMARY,
+  COLOR_LIGHT_GRAY,
+  COLOR_PRIMARY_DARKER
+} from "./ThemeConstants";
 
-export const backgroundColor = theme('mode', {
-	light: COLOR_LIGHT_GRAY,
-	dark: COLOR_DARK
+export const backgroundColor = theme("mode", {
+  light: COLOR_LIGHT_GRAY,
+  dark: COLOR_DARK
 });
 
-const textColor = theme('mode', { light: COLOR_DARK, dark: COLOR_LIGHT });
-const fontFamily = theme('font', {
-	sanSerif: FONT_SAN_SERIF,
-	monoSpaced: FONT_MONO_SPACED
+const textColor = theme("mode", { light: COLOR_DARK, dark: COLOR_LIGHT });
+const fontFamily = theme("font", {
+  sanSerif: FONT_SAN_SERIF,
+  monoSpaced: FONT_MONO_SPACED
 });
 
 const GlobalStyle = createGlobalStyle`
@@ -61,16 +62,19 @@ const GlobalStyle = createGlobalStyle`
 		color: rgb(255, 255, 255);
 		background-color: rgb(87, 62, 222);
 	}
-
 `;
 
-const Theme = props => (
-	<ThemeProvider
-		theme={{ mode: mode => mode.light, font: font => font.sanSerif }}
-	>
-		<GlobalStyle />
-		{props.children}
-	</ThemeProvider>
+const Theme = ({ children }) => (
+  <ThemeProvider
+    theme={{ mode: mode => mode.light, font: font => font.sanSerif }}
+  >
+    <GlobalStyle />
+    {children}
+  </ThemeProvider>
 );
+
+Theme.propTypes = {
+  children: PropTypes.element.isRequired
+};
 
 export default Theme;
--- src/components/writeUp/WriteUp.js
+++ src/components/writeUp/WriteUp.js
@@ -1,24 +1,26 @@
-import { Editor, EditorState } from 'draft-js';
-import React from 'react';
+import { Editor, EditorState } from "draft-js";
+import React from "react";
 
-import styled from 'styled-components/macro';
-import { COLOR_LIGHT } from '../styles/ThemeConstants';
+// eslint-disable-next-line no-unused-vars
+import { css } from "styled-components/macro";
+import { COLOR_LIGHT } from "../styles/ThemeConstants";
 
 function WriteUp() {
-	const [editorState, setEditorState] = React.useState(
-		EditorState.createEmpty()
-	);
-	return (
-		<div
-			css={`
-				background: ${COLOR_LIGHT};
-				width: 1200px;
-				margin-left: -50%;
-			`}
-		>
-			<Editor editorState={editorState} onChange={setEditorState} />
-		</div>
-	);
+  const [editorState, setEditorState] = React.useState(
+    EditorState.createEmpty()
+  );
+  return (
+    <div
+      css={`
+        background: ${COLOR_LIGHT};
+        width: 1200px;
+        margin-left: -50%;
+        min-height: 500px;
+      `}
+    >
+      <Editor editorState={editorState} onChange={setEditorState} />
+    </div>
+  );
 }
 
 export default WriteUp;
