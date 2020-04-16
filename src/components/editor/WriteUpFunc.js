ce7e273abe784733822776e648aebd63ee5a1d7a
abheist <abhishek291093@gmail.com>
2020-04-16 21:31:23 +0530

    🚧 added mutations and some config settings indentation changes

--- src/components/editor/WriteUpFunc.js
+++ src/components/editor/WriteUpFunc.js
@@ -1,21 +1,20 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/click-events-have-key-events,no-console */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useRef, useState, useEffect } from 'react';
-import {
-	Editor,
-	EditorState,
-	RichUtils,
-	convertToRaw,
-	convertFromRaw,
-} from 'draft-js';
-import { getBlockStyle, styleMap } from './editorUtils';
-import BlockStyleControls from './BlockStyleControls';
-import InlineStyleControls from './InlineStyleControls';
+import { useMutation } from '@apollo/client';
+import { convertFromRaw, convertToRaw, Editor, EditorState, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { QL_MUTATION_CREATE_ARTICLE, QL_MUTATION_UPDATE_ARTICLE } from '../../graphql/articles';
+import BlockStyleControls from './BlockStyleControls';
 import './Draft.css';
+import { getBlockStyle, styleMap } from './editorUtils';
+import InlineStyleControls from './InlineStyleControls';
 
 function WriteUpFunc() {
 	const content = window.localStorage.getItem('content');
+
+	const [createDraft, { data: articleData }] = useMutation(QL_MUTATION_CREATE_ARTICLE);
+	const [updateDraft, { data: updatedData }] = useMutation(QL_MUTATION_UPDATE_ARTICLE);
 
 	const [editorState, setEditorState] = useState(
 		content
@@ -33,9 +32,7 @@ function WriteUpFunc() {
 	}, []);
 
 	useEffect(() => {
-		const dataToSave = JSON.stringify(
-			convertToRaw(editorState.getCurrentContent())
-		);
+		const dataToSave = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
 		window.localStorage.setItem('content', dataToSave);
 	}, [editorState]);
 
@@ -66,23 +63,15 @@ function WriteUpFunc() {
 	if (!editorState) return <div>Content Loading...</div>;
 	return (
 		<div className="RichEditor-root">
-			<BlockStyleControls
-				editorState={editorState}
-				onToggle={toggleBlockType}
-			/>
-			<InlineStyleControls
-				editorState={editorState}
-				onToggle={toggleInlineStyle}
-			/>
+			<BlockStyleControls editorState={editorState} onToggle={toggleBlockType} />
+			<InlineStyleControls editorState={editorState} onToggle={toggleInlineStyle} />
 			<div className={className} onClick={focusEditor}>
 				<Editor
 					blockStyleFn={getBlockStyle}
 					customStyleMap={styleMap}
 					editorState={editorState}
 					handleKeyCommand={handleKeyCommand}
-					onChange={(newEditorState) =>
-						setEditorState(newEditorState)
-					}
+					onChange={(newEditorState) => setEditorState(newEditorState)}
 					ref={editor}
 					placeholder="Tell a story..."
 					spellCheck
