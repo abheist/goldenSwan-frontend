29fa5ca0e8ac622cb9e82d164935952e381cb77f
abheist <abhishek291093@gmail.com>
2020-04-17 14:53:02 +0530

    added teh console.log on error for apollo client

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,14 +11,14 @@ import { AUTH_TOKEN_NAME } from '../../helpers/authService';
 import Theme from '../styles/Theme';
 
 function App() {
-	const httpLink = new HttpLink({ uri: 'http://localhost:8000/graphql/' });
+	const httpLink = new HttpLink({ uri: 'http://127.0.0.1:8000/graphql/' });
 
 	const authLink = setContext((_, { headers }) => {
 		const localToken = localStorage.getItem(AUTH_TOKEN_NAME);
 		return {
 			headers: {
 				...headers,
-				authorization: localToken ? `JWT ${localToken}` : undefined,
+				Authorization: localToken ? `JWT ${localToken}` : undefined,
 			},
 		};
 	});
@@ -26,6 +26,9 @@ function App() {
 	const client = new ApolloClient({
 		cache: new InMemoryCache(),
 		link: authLink.concat(httpLink),
+		onError: (e) => {
+			console.log(e);
+		},
 	});
 
 	return (
