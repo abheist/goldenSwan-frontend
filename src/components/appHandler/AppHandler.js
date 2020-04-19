7e27d590d99b81ee73a15a87a12b1ad2582402e1
abheist <abhishek291093@gmail.com>
2020-04-19 13:57:37 +0530

    🧞‍♂️ removed the unwanted refresh ken change code, as it'll be handled now on if the server replies with token expired. otherwise it'll not refresh the tojken on every page refresh

--- src/components/appHandler/AppHandler.js
+++ src/components/appHandler/AppHandler.js
@@ -1,55 +1,11 @@
-import { useMutation } from '@apollo/client';
-import React, { useEffect, useState } from 'react';
-import { QL_MUTATION_AUTH_REFRESH_TOKEN } from '../../graphql/mutations/authentication';
-import {
-	getLocalExpTime,
-	getLocalRefreshToken,
-	getLocalToken,
-	setLocalExpTime,
-	setLocalRefreshToken,
-	setLocalToken,
-} from '../../helpers/authService';
-import { CONTACT_ADMIN_EMAIL } from '../../helpers/constants';
+import React, { useState } from 'react';
+import { getLocalToken } from '../../helpers/authService';
 import AuthenticatedApp from './AuthenticatedApp';
 import UnauthenticatedApp from './UnauthenticatedApp';
 
 function AppHandler() {
 	const [token, setToken] = useState(getLocalToken() || undefined);
-	const [counter, setCounter] = useState(0);
-	const [doRefreshToken, { data: refreshTokenData, error }] = useMutation(
-		QL_MUTATION_AUTH_REFRESH_TOKEN
-	);
 
-	setInterval(() => {
-		setCounter(counter + 1);
-	}, 240000);
-
-	useEffect(() => {
-		if (getLocalExpTime()) {
-			doRefreshToken({
-				variables: {
-					refreshToken: getLocalRefreshToken(),
-				},
-			});
-		}
-	}, [doRefreshToken, counter]);
-
-	useEffect(() => {
-		if (refreshTokenData?.refreshToken?.success) {
-			setLocalToken(refreshTokenData.refreshToken.token);
-			setLocalRefreshToken(refreshTokenData.refreshToken.refreshToken);
-			setLocalExpTime(refreshTokenData.refreshToken.payload.exp);
-		}
-	}, [refreshTokenData]);
-
-	if (error) {
-		return (
-			<div>
-				Server error!!! Please contact
-				<a href={`mailto:${CONTACT_ADMIN_EMAIL}`}> admin</a>
-			</div>
-		);
-	}
 	return <>{token ? <AuthenticatedApp /> : <UnauthenticatedApp setToken={setToken} />}</>;
 }
 
