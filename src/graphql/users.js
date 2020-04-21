8a64f851590378fd36494a70e62a2adff30e8d10
abheist <abhishek291093@gmail.com>
2020-04-21 22:52:02 +0530

    🚧 users gql to get the user detail and articles

--- src/graphql/users.js
+++ src/graphql/users.js
@@ -1,32 +1,36 @@
 import { gql } from '@apollo/client';
 
-export const QL_QUERY_USERS = gql`
-    users {
-        id
-        username
-        firstName
-        lastName
-        isActive
-    }
+// export const QL_QUERY_USERS = gql`
+//     users {
+//         id
+//         username
+//         firstName
+//         lastName
+//         isActive
+//     }
+// `;
+
+const QL_QUERY_PROFILE = gql`
+	query user($userId: ID!) {
+		user(userId: $userId) {
+			id
+			username
+			firstName
+			lastName
+			isActive
+			articleSet {
+				slug
+				title
+				subtitle
+				content
+				created
+				updated
+				published
+			}
+		}
+	}
 `;
 
-export const QL_QUERY_USER = gql`
-    user {
-        id
-        username
-        firstName
-        lastName
-        isActive
-        articleSet {
-            slug
-            title
-            subtitle
-            content
-            created
-            updated
-            published
-        }
-    }
-`;
+export default QL_QUERY_PROFILE;
 
-export const QL_QUERY_USER_SEARCH = gql``;
+// export const QL_QUERY_USER_SEARCH = gql``;
