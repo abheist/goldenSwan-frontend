547e7f98e83747894197e455ae41473ee22c7a10
abheist <abhishek291093@gmail.com>
2020-04-24 00:16:30 +0530

    🚧 Edit profile, added link in profile to edit profile

--- src/components/profile/profileHeader.js
+++ src/components/profile/profileHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Facebook, Instagram, Linkedin, Twitter } from 'styled-icons/feather';
+import { Link } from 'react-router-dom';
 import { Flex } from '../styles/Flex';
 import IconButton from '../styles/IconButton';
 import { MeBody2, MeH4 } from '../styles/Typography';
 import Tooltip from '../tooltip/Tooltip';
 import { ProfilePic } from './profileComponents';
+import MeButton from '../styles/MeButton';
 
 function ProfileHeader({ firstname, lastname, bio, profilePic }) {
 	return (
@@ -17,7 +19,7 @@ function ProfileHeader({ firstname, lastname, bio, profilePic }) {
 				On a path to solve one of the major global issues. Obsessed with all things related
 				to creativity. Full-Stack Developer and a Designer.
 			</MeBody2>
-			<Flex width="200px" gapBetween="5">
+			<Flex gapBetween="5" align="center" justify="center">
 				<Tooltip title="Facebook" placement="bottom">
 					<IconButton>
 						<Facebook size="16px" />
@@ -38,6 +40,9 @@ function ProfileHeader({ firstname, lastname, bio, profilePic }) {
 						<Linkedin size="16px" />
 					</IconButton>
 				</Tooltip>
+				<Link to="edit-profile">
+					<MeButton>Edit profile</MeButton>
+				</Link>
 			</Flex>
 		</Flex>
 	);
