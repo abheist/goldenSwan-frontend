0e6f25b1ac8a2bb105854bfc208a962552acc515
abheist <abhishek291093@gmail.com>
2020-04-28 22:26:31 +0530

    🐛 update appHandler token once the token changes in localStorage

--- src/components/appHandler/AppHandler.js
+++ src/components/appHandler/AppHandler.js
@@ -6,6 +6,12 @@ import UnauthenticatedApp from './UnauthenticatedApp';
 function AppHandler() {
 	const [token, setToken] = useState(getLocalToken() || undefined);
 
+	window.addEventListener('storage', () => {
+		if (token !== getLocalToken()) {
+			setToken(getLocalToken());
+		}
+	});
+
 	return <>{token ? <AuthenticatedApp /> : <UnauthenticatedApp setToken={setToken} />}</>;
 }
 
