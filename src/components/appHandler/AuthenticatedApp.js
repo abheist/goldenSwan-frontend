ce40839dc968f6b2df3b40de04aae20febd84fc2
abheist <abhishek291093@gmail.com>
2020-05-13 21:51:07 +0530

    🚧 body width to 700

--- src/components/appHandler/AuthenticatedApp.js
+++ src/components/appHandler/AuthenticatedApp.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components/macro';
 import CreateArticle from '../articles/CreateArticle';
 import Header from '../header/Header';
 import Home from '../home/Home';
@@ -11,7 +13,11 @@ import ViewArticle from '../articles/ViewArticle';
 function AuthenticatedApp() {
 	return (
 		<Flex flex="1" justify="center">
-			<div>
+			<div
+				css={`
+					max-width: 700px;
+				`}
+			>
 				<Header />
 				<Flex justify="center" width="100%">
 					<FlexItem>
