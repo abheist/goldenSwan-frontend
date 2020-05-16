5600fb091b67e4b45f25d4b2b2b1dd652cc47673
abheist <abhishek291093@gmail.com>
2020-05-16 21:57:50 +0530

    🚧 updated the user context to handle update

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { getLocalUsername, setLocalUsername } from '../helpers/authService';
 
-const UserContext = React.createContext(undefined);
+const UserContext = React.createContext();
+const initialState = getLocalUsername() || undefined;
+
+const updatedContextAndLocalUsername = (oldState, newState) => {
+	setLocalUsername(newState);
+	return newState;
+};
+
+export function UserContextProvider(props) {
+	const [username, dispatch] = useReducer(updatedContextAndLocalUsername, initialState);
+	const value = { username, dispatch };
+
+	return <UserContext.Provider value={value}>{props.children}</UserContext.Provider>;
+}
 
 export default UserContext;
