019f06f8774e7bdd233bca7f85c16423c8a577ae
abheist <abhishek291093@gmail.com>
2020-05-21 01:28:12 +0530

    🚧 profile pic, change the component to input and upload images and hide the default view

--- src/components/profile/profilePic.js
+++ src/components/profile/profilePic.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/macro';
 
-const ProfilePic = styled.div`
+const ProfilePic = styled.input`
 	height: 120px;
 	width: 120px;
 	border-radius: 100%;
@@ -8,6 +8,18 @@ const ProfilePic = styled.div`
 	background-position: center;
 	background-size: cover;
 	background-repeat: no-repeat;
+	cursor: pointer;
+	transition: all 0.03s;
+	box-sizing: border-box;
+	border: 0;
+
+	&:active {
+		box-shadow: inset 0 0 0px 5px white;
+	}
+
+	& ::-webkit-file-upload-button {
+		display: none;
+	}
 `;
 
 export default ProfilePic;
