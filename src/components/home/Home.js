345a7ea8245d5c678cecf854c39b0c9ca7615d88
abheist <abhishek291093@gmail.com>
2020-06-06 01:40:34 +0530

    🏡 Empty state

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,14 @@
-import React, { useState, useContext, useEffect } from 'react';
-// import WriteBox from '../writeBox/writeBox';
 import { useQuery } from '@apollo/client';
-import { MeH4 } from '../styles/Typography';
-import { QL_QUERY_PROFILE } from '../../graphql/users';
-
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ArrowNarrowRight } from '@styled-icons/heroicons-solid/ArrowNarrowRight';
 import UserContext from '../../contexts/UserContext';
+import { QL_QUERY_PROFILE } from '../../graphql/users';
 import BlogList from '../profile/blogList';
+import { MeH4 } from '../styles/Typography';
+import UnboxingDoodle from '../../assets/img/svg/UnboxingDoodle.svg';
+import { Flex } from '../styles/Flex';
+import MeButton from '../styles/MeButton';
 
 function Home() {
 	const [unPublished, setUnPublished] = useState([]);
@@ -35,10 +38,40 @@ function Home() {
 	return (
 		<>
 			{/* <WriteBox /> */}
-			<MeH4>Unpublished articles:</MeH4>
-			<div>
-				{unPublished.length ? <BlogList blogs={unPublished} /> : <div>something...</div>}
-			</div>
+			{!unPublished.length ? (
+				<Flex direction="column" justify="center" align="center" margin={{ top: 140 }}>
+					<img
+						src={UnboxingDoodle}
+						alt="MeUp vector showing a persons"
+						css={`
+							width: 32%;
+							max-width: 500px;
+							position: absolute;
+							left: 10%;
+							bottom: 0;
+						`}
+					/>
+					<Link to="write">
+						<MeButton kind="primary">
+							Let it out <ArrowNarrowRight size="24" />
+						</MeButton>
+					</Link>
+					<h2>Get ready and let it out.</h2>
+					<p>“The first draft is just you telling yourself the story.”</p>
+					<p>― Terry Pratchett</p>
+				</Flex>
+			) : (
+				<>
+					<MeH4>Unpublished articles:</MeH4>
+					<div>
+						{unPublished.length ? (
+							<BlogList blogs={unPublished} />
+						) : (
+							<div>something...</div>
+						)}
+					</div>
+				</>
+			)}
 		</>
 	);
 }
